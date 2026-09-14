a7b38c1fa253301f7c38f8cc66849fb61a025a97
Azizbek <azizbekreimbaev1003@gmail.com>
2026-09-14 13:47:46 +0900

    feat: home and property List component publishing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,10 @@ import { CssBaseline } from "@mui/material";
 import { light } from "../scss/MaterialTheme";
 import { useState } from "react";
 import '../scss/app.scss'
-import '../scss/pc/main.scss'
+import "../scss/app.scss";
+import "../scss/pc/main.scss";
+import "swiper/css/bundle";
+
 
 export default function App({ Component, pageProps }: AppProps) {
   // @ts-ignore
